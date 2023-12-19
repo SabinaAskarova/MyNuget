42f2185de57841116fa9a221867a0fb509bd7e95
SabinaAskarova <sabinaaskarova56@gmail.com>
2023-12-19 12:03:26 +0400

    Add project files.

---
+++ MyFirstNugetPack/SumTwoNumbers.cs
@@ -0,0 +1,20 @@
+﻿namespace MyFirstNugetPack
+{
+    public class SumTwoNumbers
+    {
+        public int Sum(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Subtraction(int number1,int number2)
+        {
+            return number1 - number2;
+        }
+
+        public static void AboutMe()
+        {
+            Console.WriteLine("Hi, My Name is Sabina! I am .Net developer...");
+        }
+    }
+}
